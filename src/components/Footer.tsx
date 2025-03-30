80e14ff21732d77e8939f06f43abcfa875cc3301
spencerhum04 <spencerhum04@gmail.com>
2025-03-30 08:13:09 -0400

    feat: footer copyright

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,7 @@
 export default function Footer() {
     return (
-        <div className="w-full px-6 sm:px-32 h-10">Footer</div>
+        <div className="w-full px-6 sm:px-32 h-10">
+            <div className="text-sm text-center">Copyright 2025 Symbios Bio-Suite. All rights reserved.</div>
+        </div>
     )
 }
