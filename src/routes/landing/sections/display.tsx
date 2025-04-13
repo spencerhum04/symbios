84d4c175dfc06299764256e80109751c9a615f6e
spencerhum04 <spencerhum04@gmail.com>
2025-04-13 13:32:40 -0400

    fix: component body

--- src/routes/landing/sections/display.tsx
+++ src/routes/landing/sections/display.tsx
@@ -6,9 +6,9 @@ export default function Display() {
         <div className="flex flex-col max-w-[1024px] w-full mx-auto gap-20 text-center sm:text-left">
             <div className="text-4xl sm:text-5xl w-full font-medium">Engineered for modern product teams</div>
             <div className="flex flex-row items-center justify-between overflow-hidden">
-                <ExpandableCard image={arm} title={"Purpose-built for product development"} body={"Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum"} />
-                <ExpandableCard image={arm} title={"Designed to move fast"} body={"Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum"} />
-                <ExpandableCard image={arm} title={"Crafted to perfection"} body={"Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum"} />
+                <ExpandableCard image={arm} title={"Purpose-built for product development"} />
+                <ExpandableCard image={arm} title={"Designed to move fast"} />
+                <ExpandableCard image={arm} title={"Crafted to perfection"} />
             </div>
         </div>
     )
