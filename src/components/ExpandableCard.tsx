722ebcb4baeff3b209089ee21aa9b63e32d8eae9
spencerhum04 <spencerhum04@gmail.com>
2025-04-13 13:26:36 -0400

    feat: blur on expandable card component

--- src/components/ExpandableCard.tsx
+++ src/components/ExpandableCard.tsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
+
 export default function ExpandableCard({ image, title, body } : { image: string, title: String, body: String }) {
+
+    const [hovered, setHovered] = useState(false);
+
     return (
-        <div className="w-84 h-90 bg-black rounded-xl px-6 py-8">
-            <div className="flex flex-col items-center justify-between h-full">
-                <img src={image} className="opacity-50 hover:opacity-60 h-56" />
+        <div className="relative w-84 h-90 bg-black rounded-xl px-6 py-8" onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
+            {hovered && (
+                <div className="absolute inset-4 rounded-xl bg-neutral-800/30 backdrop-blur-md transition-all duration-300" />
+            )}
+            <div className="relative z-10 flex flex-col items-center justify-between h-full">
+                <img src={image} className={`h-56 transition-opacity duration-200 ${hovered ? "opacity-60" : "opacity-50"}`} />
                 <div className="flex flex-row justify-between items-end gap-x-8 w-full">
                     <div className="text-xl font-medium">{title}</div>
-                    <div className="shrink-none">
+                    <div className="shrink-none rounded-full">
                         <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="#8a8f98" viewBox="0 0 256 256"><path d="M128,28A100,100,0,1,0,228,128,100.11,100.11,0,0,0,128,28Zm0,192a92,92,0,1,1,92-92A92.1,92.1,0,0,1,128,220Zm44-92a4,4,0,0,1-4,4H132v36a4,4,0,0,1-8,0V132H88a4,4,0,0,1,0-8h36V88a4,4,0,0,1,8,0v36h36A4,4,0,0,1,172,128Z"></path></svg>
                     </div>
                 </div>
--- src/routes/landing/sections/display.tsx
+++ src/routes/landing/sections/display.tsx
@@ -1,17 +1,10 @@
-import Card from "../../../components/Card";
-import model from "../../../assets/model.png"
 import ExpandableCard from "../../../components/ExpandableCard";
 import arm from "../../../assets/arm.png";
 
 export default function Display() {
     return (
-        <div className="flex flex-col max-w-[1024px] w-full mx-auto gap-8 text-center sm:text-left">
+        <div className="flex flex-col max-w-[1024px] w-full mx-auto gap-20 text-center sm:text-left">
             <div className="text-4xl sm:text-5xl w-full font-medium">Engineered for modern product teams</div>
-            <div className="flex flex-row items-center gap-6 justify-between overflow-hidden">
-                <Card image={model} title={"Title"} body={"Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum"} />
-                <Card image={model} title={"Title"} body={"Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum"} />
-                <Card image={model} title={"Title"} body={"Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum"} />
-            </div>
             <div className="flex flex-row items-center justify-between overflow-hidden">
                 <ExpandableCard image={arm} title={"Purpose-built for product development"} body={"Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum"} />
                 <ExpandableCard image={arm} title={"Designed to move fast"} body={"Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum"} />
