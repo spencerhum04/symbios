972bc6ce16158e46486dff7b6c74470b4ef3d40b
spencerhum04 <spencerhum04@gmail.com>
2025-04-17 17:48:23 -0400

    fix: logos

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from "react"
-import logo from "../assets/logo.png"
+import logo_full_white from "../assets/logo-full-white.png"
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
@@ -44,7 +44,7 @@ export default function Navbar() {
         <div className="bg-dark w-full h-16 px-6 place-content-center border-b border-neutral-800">
             <div className="flex flex-row items-center justify-between max-w-[1024px] w-full mx-auto">
                 <div className="flex flex-row items-center justify-between w-full sm:w-25">
-                    <img src={logo} className="h-5" />
+                    <img src={logo_full_white} className="h-7" />
                     <button className={`sm:hidden w-[16px] h-[12px] rounded-lg flex flex-col justify-between`} onClick={() => {setMenu(true)}}>
                         <span className="w-full h-0.5 bg-white"></span>
                         <span className="w-full h-0.5 bg-white"></span>
@@ -65,7 +65,7 @@ export default function Navbar() {
 
             <div ref={sideRef} className={`top-0 left-0 absolute z-10 bg-neutral-900 w-full duration-500 ease-out transition-transform ${menu ? "translate-y-0" : "-translate-y-full"}`}>
                 <div className="flex flex-row items-center justify-between w-full px-6 pt-4">
-                    <img src={logo} className="h-5" />
+                    <img src={logo_full_white} className="h-5" />
                     <button className={`sm:hidden w-[20px] h-[20px] rounded-lg flex flex-col justify-between`} onClick={() => {setMenu(false)}}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="white" viewBox="0 0 256 256"><path d="M205.66,194.34a8,8,0,0,1-11.32,11.32L128,139.31,61.66,205.66a8,8,0,0,1-11.32-11.32L116.69,128,50.34,61.66A8,8,0,0,1,61.66,50.34L128,116.69l66.34-66.35a8,8,0,0,1,11.32,11.32L139.31,128Z"></path></svg>
                     </button>
