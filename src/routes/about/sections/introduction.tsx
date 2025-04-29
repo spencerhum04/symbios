3bcd1c166ab93bc1187f4f67933b0af1458eb25f
spencerhum04 <spencerhum04@gmail.com>
2025-04-29 11:52:32 -0400

    fix: about intro

--- src/routes/about/sections/introduction.tsx
+++ src/routes/about/sections/introduction.tsx
@@ -2,7 +2,7 @@ export default function Introduction() {
     return (
         <div className="flex flex-col text-center flex flex-col gap-4">
             <div className="text-4xl font-medium">Bringing magic back to prosthetics.</div>
-            <div className="text-xl">A priority in efficiency and accessibility.</div>
+            <div className="text-xl">A student-led project into the priority of efficiency and accessibility.</div>
         </div>
     )
 }
