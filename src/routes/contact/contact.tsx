680e851c9227e10b4f38c7605838474a5607dcda
spencerhum04 <spencerhum04@gmail.com>
2025-04-30 23:33:17 -0400

    feat: contact form

--- src/routes/contact/contact.tsx
+++ src/routes/contact/contact.tsx
@@ -1,4 +1,5 @@
 import Footer from "../../components/Footer";
+import Form from "../../components/Form";
 import Navbar from "../../components/Navbar";
 import Hero from "./sections/hero";
 
@@ -8,10 +9,9 @@ export default function Contact() {
             <div className="w-full fixed z-50">
                 <Navbar />
             </div>
-            <div className="flex flex-col gap-40 py-20">
-                <div className="px-6 sm:px-0 pt-16">
-                    <Hero />
-                </div>
+            <div className="flex flex-col sm:flex-row sm:justify-between gap-10 pt-36 pb-20 max-w-[1024px] mx-auto px-6 sm:px-0">
+                <Hero />
+                <Form />
             </div>
             <Footer />
         </>
--- src/routes/contact/sections/hero.tsx
+++ src/routes/contact/sections/hero.tsx
@@ -1,10 +1,8 @@
 export default function Hero() {
     return (
-        <div className="px-6 sm:px-32">
-            <div className="flex flex-col text-center flex flex-col gap-4">
-                <div className="text-4xl font-semibold">How can we help?</div>
-                <div className="text-xl text-stone">Get in touch with our team to learn more or check out our FAQ.</div>
-            </div>
+        <div className="flex flex-col text-center sm:text-left gap-4">
+            <div className="text-4xl font-semibold">How can we help?</div>
+            <div className="text-xl text-stone">Your feedback helps us improve and we'll get back to you in 24h.</div>
         </div>
     )
 }
